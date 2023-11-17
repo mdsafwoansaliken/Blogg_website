045f15940acb45fcb88224fac93d54736f4098ff
Aryan Alam <aryan138alam@gmail.com>
2023-11-18 04:20:19 +0600

    Fetching

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,17 +1,35 @@
 import HomePosts from "../components/HomePosts"
 import Navbar from "../components/Navbar"
 import Footer from "../components/Footer"
+import axios from "axios"
+import { URL } from "../url"
+import { useEffect, useState } from "react"
 
 const Home = () => {
+
+  const [posts,setPosts]=useState([])
+
+  const fetchPosts=async()=>{
+    try{
+      const res=await axios.get(URL+"/api/posts/")
+      // console.log(res.data)
+      setPosts(res.data)
+    }
+    catch(err){
+      console.log(err)
+    }
+  }
+
+  useEffect(()=>{
+    fetchPosts()
+  })
   return (
     <>
     <Navbar/>
     <div className="px-8 md:px-[200px]">
-      <HomePosts/>
-      <HomePosts/>
-      <HomePosts/>
-      <HomePosts/>
-      <HomePosts/>
+      {posts.map((post)=>(
+        <HomePosts key={post._id} post={post}/>
+      ))}
     </div>
     <Footer/>
     </>
