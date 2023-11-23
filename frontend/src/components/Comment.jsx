0bb0b2bde63dbce7a2f0b9b9a30348a95e9aa429
Aryan Alam <aryan138alam@gmail.com>
2023-11-23 16:41:54 +0600

    Front end corrected

--- frontend/src/components/Comment.jsx
+++ frontend/src/components/Comment.jsx
@@ -6,7 +6,7 @@ import { useContext, useState } from "react";
 //import { UserContext } from "../context/UserContext";
 import { useUser } from "../context/UserContext";
 
-const Comment = ({ c, post, postUpdateComment, postDeleteComment }) => {
+const Comment = ({ c, postUpdateComment, postDeleteComment }) => {
   const { user } = useUser();
   const [isEditing, setIsEditing] = useState(false);
   const [editedComment, setEditedComment] = useState(c.comment);
@@ -28,7 +28,6 @@ const Comment = ({ c, post, postUpdateComment, postDeleteComment }) => {
         { withCredentials: true }
       );
 
-      // Check if the update was successful
       if (response.status === 200) {
         postUpdateComment(id, editedComment);
         setIsEditing(false);
@@ -40,37 +39,59 @@ const Comment = ({ c, post, postUpdateComment, postDeleteComment }) => {
   };
 
   return (
-    <div className="px-2 py-2 bg-gray-200 rounded-lg my-2">
+    <div className="px-4 py-3 bg-gray-100 rounded-lg my-3 border border-gray-200">
       <div className="flex items-center justify-between">
-        <h3 className="font-bold text gray-600">@{c.author}</h3>
-        <div className="flex justify-center items-center space-x-4">
-          <p>{new Date(c.updatedAt).toString().slice(0, 15)}</p>
-          <p>{new Date(c.updatedAt).toString().slice(16, 24)}</p>
-          {user?._id === c?.userId ? (
+        <h3 className="font-bold text-gray-600">@{c.author}</h3>
+        <div className="flex items-center space-x-4 text-xs text-gray-500">
+          <p>{new Date(c.updatedAt).toDateString()}</p>
+          {user?._id === c?.userId && (
             isEditing ? (
-              <div className="flex items-center justify-center space-x-2">
+              <div className="flex items-center space-x-2">
                 <input
                   type="text"
                   value={editedComment}
                   onChange={(e) => setEditedComment(e.target.value)}
+                  className="border-b border-gray-400 focus:outline-none"
                 />
-                <button onClick={() => editComment(c._id)}>Save</button>
-                <button onClick={() => setIsEditing(false)}>Cancel</button>
+                <button
+                  onClick={() => editComment(c._id)}
+                  className="bg-green-500 text-white px-2 py-1 rounded hover:bg-green-600 focus:outline-none"
+                >
+                  Save
+                </button>
+                <button
+                  onClick={() => setIsEditing(false)}
+                  className="text-red-500 hover:text-red-600 focus:outline-none"
+                >
+                  Cancel
+                </button>
               </div>
             ) : (
-              <div className="flex items-center justify-center space-x-2">
-                <p onClick={() => setIsEditing(true)}><BiEdit /></p>
-                <p className="cursor-pointer" onClick={() => deleteComment(c._id)}>
+              <div className="flex items-center space-x-2">
+                <p
+                  className="cursor-pointer text-gray-400 hover:text-blue-500"
+                  onClick={() => setIsEditing(true)}
+                >
+                  <BiEdit />
+                </p>
+                <p
+                  className="cursor-pointer text-gray-400 hover:text-red-500"
+                  onClick={() => deleteComment(c._id)}
+                >
                   <MdDelete />
                 </p>
               </div>
             )
-          ) : (
-            ""
           )}
         </div>
       </div>
-      <p className="px-4 mt-2">{isEditing ? "Editing..." : c.comment}</p>
+      <p className="mt-2">
+        {isEditing ? (
+          "Editing..."
+        ) : (
+          <span className="text-gray-700">{c.comment}</span>
+        )}
+      </p>
     </div>
   );
 };
