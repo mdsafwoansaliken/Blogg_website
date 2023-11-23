550a03f79470adbd6ced00f2489cca905e55f701
mdsafwoansaliken <md.safwoan.saliken@gmail.com>
2023-11-23 18:35:41 +0600

    create post image implemented

--- frontend/src/pages/CreatePost.jsx
+++ frontend/src/pages/CreatePost.jsx
@@ -1,71 +1,82 @@
-import { useContext, useState } from "react"
-import Footer from "../components/Footer"
-import Navbar from "../components/Navbar"
-import {ImCross} from "react-icons/im"
-import { UserContext } from "../context/UserContext"
-import { useUser } from "../context/UserContext"
-import { URL } from "../url"
-import axios from 'axios'
-import { Navigate, useNavigate } from "react-router-dom"
+import { useContext, useState } from "react";
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
+import { ImCross } from "react-icons/im";
+import { UserContext } from "../context/UserContext";
+import { useUser } from "../context/UserContext";
+import { URL } from "../url";
+import axios from 'axios';
+import { Navigate, useNavigate } from "react-router-dom";
 
 const CreatePost = () => {
+  const [title, setTitle] = useState("");
+  const [desc, setDesc] = useState("");
+  const [file, setFile] = useState(null);
+  const { user } = useUser();
+  const [cat, setCat] = useState('');
+  const [cats, setCats] = useState([]);
+  const navigate = useNavigate();
 
-    const [title, setTitle] = useState("")
-    const [desc, setDesc] = useState("")
-    const [file, setFile] = useState(null)
-    const {user} = useUser();
-    const[cat,setCat]=useState('')
-    const [cats,setCats]=useState([])
-    const navigate = useNavigate()
+  const deleteCategory = (i) => {
+    let updatedCats = [...cats];
+    updatedCats.splice(i);
+    setCats(updatedCats);
+  };
 
-    const deleteCategory=(i)=> {
-        let updatedCats=[...cats]
-        updatedCats.splice(i)
-        setCats(updatedCats)
+  const addCategory = () => {
+    let updatedCats = [...cats];
+    updatedCats.push(cat);
+    setCat('');
+    setCats(updatedCats);
+  };
+
+  const handleFileChange = (e) => {
+    const selectedFile = e.target.files[0];
+
+    // Create a FileReader
+    const reader = new FileReader();
+
+    // Set a callback for when the file is loaded
+    reader.onloadend = () => {
+      // Set the file and preview
+      setFile({
+        file: selectedFile,
+        preview: reader.result,
+      });
+    };
+
+    // Read the file as a data URL, triggering the callback
+    reader.readAsDataURL(selectedFile);
+  };
+
+  const handleCreate = async (e) => {
+    e.preventDefault();
+    const post = {
+      title,
+      desc,
+      username: user.username,
+      userId: user._id,
+      categories: cats,
+    };
+
+    if (file) {
+      const data = new FormData();
+      const filename = Date.now() + file.file.name;
+      data.append("img", filename);
+      data.append("file", file.file);
+      post.photo = filename;
+
+      try {
+        const imgUpload = await axios.post(URL + "/api/upload", data);
+      } catch (err) {
+        console.log(err);
+      }
     }
-
-    const addCategory=()=> {
-        let updatedCats=[...cats]
-        updatedCats.push(cat)
-        setCat('')
-        setCats(updatedCats)
-    }   
-    
-    const handleCreate = async (e) =>{
-      e.preventDefault()
-      const post = {
-        title,
-        desc,
-        username: user.username,
-        userId: user._id,
-        categories: cats
-      }
-
-      if(file){
-        const data = new FormData()
-        const filename = Date.now() + file.name
-        data.append("img", filename)
-        data.append("file", file)
-        post.photo = filename
-
-        try{
-          const imgUpload = await axios.post(URL+"/api/upload", data)
-          
-        }
-        catch(err){
-          console.log(err)
-        }
-      }
-      try{
-        const res = await axios.post(URL+"/api/posts/create", post, {withCredentials:true})
-        navigate("/posts/post/"+res.data._id)
-      }
-      catch(err){
-        
-      }
-
-    }
-
+    try {
+      const res = await axios.post(URL + "/api/posts/create", post, { withCredentials: true });
+      navigate("/posts/post/" + res.data._id);
+    } catch (err) {}
+  };
 
   return (
     <div>
@@ -80,10 +91,18 @@ const CreatePost = () => {
             className="w-full px-4 py-2 mb-4 rounded border border-gray-300 focus:outline-none focus:border-indigo-500"
           />
           <input
-            onChange={(e) => setFile(e.target.files[0])}
+            onChange={handleFileChange}
             type="file"
             className="w-full mb-4"
           />
+          {/* Image Preview */}
+          {file && (
+            <img
+              src={file.preview}
+              alt="Selected Image Preview"
+              className="max-w-[300px] max-h-[200px] mb-4"
+            />
+          )}
           <div className="flex items-center mb-4">
             <input
               value={cat}
@@ -131,7 +150,7 @@ const CreatePost = () => {
       </div>
       <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default CreatePost
+export default CreatePost;
